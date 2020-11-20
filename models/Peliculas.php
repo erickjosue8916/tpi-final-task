249e6a6afa4869795615a20133a7e9bc95b1ec8c
Erick Saravia <erickjosue8916@gmail.com>
2020-11-19 21:19:20 -0600

    feet: (filter/sort) add filter and sort to project

--- models/Peliculas.php
+++ models/Peliculas.php
@@ -1,11 +1,12 @@
 <?php
-
+/* SELECT pe.id, pe.titulo, pe.descripcion, IF (re.id_usuario = 0, 'Yes', 'No') as reaction FROM `peliculas` pe left JOIN reacciones re on re.id_pelicula = pe.id */
 require_once "database/MySqlConnection.php";
 require_once "database/IMySqlActions.php";
 class Peliculas extends MySqlConnection {
 
   const TABLE_NAME = 'peliculas';
-
+  private $filterFields = ['titulo', 'disponibilidad'];
+  private $sortFields = ['titulo', 'stock', 'precio_alquiler', 'precio_venta', 'disponibilidad'];
 
   private $titulo;
   private $descripcion;
@@ -42,15 +43,21 @@ class Peliculas extends MySqlConnection {
     parent::__construct();
   }
 
-  public function list () {
+  public function list ($page = 1, $limit = 20, $filter = [], $sort = []) {
     $result = [
       'peliculas' => [],
       'error' => ''
     ];
-
-    $sql = "SELECT * FROM " . self::TABLE_NAME;
+    $usuarioId = (isset($_COOKIE['usuario_id'])) ? $_COOKIE['usuario_id']: 0;
+    $offset = ($page - 1) * $limit;
+    $sql = "SELECT p.id, p.titulo, p.descripcion,  p.imagen, p.stock, p.precio_alquiler, p.precio_venta, p.disponibilidad, IF (re.id_usuario = $usuarioId, 'Yes', 'No') as reaction FROM " . self::TABLE_NAME . " p";
+    $sql .= " LEFT JOIN reacciones re on re.id_pelicula = p.id ";
+    $sql .= $this->createSqlFilter($filter);
+    $sql .= $this->createSqlSort($sort);
+    $sql .= " limit $limit offset $offset";
     $stmt = $this->db->prepare($sql);
-    
+    var_dump($filter);
+    $this->setPrepareValues($stmt, $filter);
     if ($stmt->execute()) {
             while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
               array_push($result['peliculas'], $row);
@@ -61,6 +68,100 @@ class Peliculas extends MySqlConnection {
     }
     return json_encode($result);
   }
+
+  private function createSqlFilter($filter) {
+    $sql = "";
+    $filters = $this->filterFields; // set available filters here
+    if (count($filter)) {
+      $i = 0;
+      foreach ($filter as $key => $value) {
+        $searchInFilters = array_search($key, $filters);
+        if ($searchInFilters === false) $searchInFilters = -1;
+        if ($searchInFilters >= 0) {
+          $sql .= ($i == 0 ) ? " WHERE " : " AND ";
+          switch ($key) {
+            case 'titulo':
+              $sql .= "p.titulo LIKE :titulo"; 
+              break;
+            case 'disponibilidad':
+              $sql .= "p.disponibilidad LIKE :disponibilidad"; 
+              break;        
+            default:
+              # code...
+              break;
+          }
+        }
+        $i++;
+      }
+    }
+    return $sql;
+  }
+
+  private function setPrepareValues ($stmt, $filter) {
+    $filters = $this->filterFields;
+    foreach ($filter as $key => $value) {
+      $searchInFilters = array_search($key, $filters);
+      if ($searchInFilters === false) $searchInFilters = -1;
+      if ($searchInFilters >= 0  ) {
+        switch ($key) {
+          case 'titulo':
+            $nombre = "%$value%";
+            $stmt->bindParam(':titulo', $nombre);
+            break;
+          case 'disponibilidad':
+            $rol = $value;
+            $stmt->bindParam(':disponibilidad', $rol);
+            break;
+          
+          default:
+            # code...
+            break;
+        }
+      }
+     // $i++;
+    }
+  }
+
+  private function createSqlSort($rules) {
+    $sql = "";
+    $fields = $this->sortFields; // set available filters here
+    if (count($rules)) {
+      $i = 0;
+      foreach ($rules as $key => $value) {
+        $searchInFilters = array_search($key, $fields);
+        if ($searchInFilters === false) $searchInFilters = -1;
+        echo "<br>";
+        if ($searchInFilters >= 0  ) {
+          $value = strtoupper($value);
+          if ($value == 'ASC' || $value == 'DESC') $sql .= ($i == 0) ? " ORDER BY " : " , ";
+          switch ($key) {
+            case 'titulo':
+              if ( $value == 'ASC' || $value == 'DESC' ) $sql .= " p.titulo " . $value ." "; 
+              break;
+            case 'stock':
+              if ( $value == 'ASC' || $value == 'DESC' ) $sql .= " p.stock " . $value ." "; 
+              break;
+            case 'precio_alquiler':
+              if ( $value == 'ASC' || $value == 'DESC' ) $sql .= " p.precio_alquiler " . $value ." "; 
+              break;
+            case 'precio_venta':
+              if ( $value == 'ASC' || $value == 'DESC' ) $sql .= " p.precio_venta " . $value ." "; 
+              break;
+            case 'disponibilidad':
+              if ( $value == 'ASC' || $value == 'DESC' ) $sql .= " p.disponibilidad " . $value ." "; 
+              break;
+            
+            default:
+              # code...
+              break;
+          }
+        }
+        $i++;
+      }
+    }
+    return $sql;
+  }
+
 
   public function create () {
     $result = [

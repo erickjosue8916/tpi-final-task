7cfc49483362b5c17a8f2b84cd00719687be20cc
EddyRC <43689872+EddyRC@users.noreply.github.com>
2020-11-20 21:03:44 -0600

    Reyes trabajando en vista peliculas

--- components/table.php
+++ components/table.php
@@ -23,15 +23,15 @@
         <?php foreach ($cols as $key => $col) { ?>
             <td scope="col"><?=$col?></td>
         <?php } ?>
-          <?php
-            if ($_COOKIE['rol'] === 'Administrador') {
-              echo "<td> <a href='" . BASE_DIR . "Clients/details&id=" . $cols['id'] . "' class=''> visualizar </a> </td>";
-              echo "<td> <a href='" . BASE_DIR . "Clients/update&id=" . $cols['id'] . "' class=''> editar </a> </td>";
-              echo "<td> <a href='" . BASE_DIR . "Clients/delete&id=" . $cols['id'] . "' class=''> eliminar </a> </td>";
-            }else {
-              echo "<td> <a href='" . BASE_DIR . "Clients/details&id=" . $cols['id'] . "' class=''> visualizar </a> </td>";
-            }
-          ?>
+          <!-- <?php
+            // if ($_COOKIE['rol'] === 'Administrador') {
+            //   echo "<td> <a href='" . BASE_DIR . "Clients/details&id=" . $cols['id'] . "' class=''> visualizar </a> </td>";
+            //   echo "<td> <a href='" . BASE_DIR . "Clients/update&id=" . $cols['id'] . "' class=''> editar </a> </td>";
+            //   echo "<td> <a href='" . BASE_DIR . "Clients/delete&id=" . $cols['id'] . "' class=''> eliminar </a> </td>";
+            // }else {
+            //   echo "<td> <a href='" . BASE_DIR . "Clients/details&id=" . $cols['id'] . "' class=''> visualizar </a> </td>";
+            // }
+          ?> -->
         </tr>
       <?php 
         
--- views/peliculasList.php
+++ views/peliculasList.php
@@ -1,7 +1,6 @@
-<div class="container-fluid contenedor text-center">
-	<h1 class="text-center">CARDS BY Francisco Kataldo</h1>
-	<div class=" container text-center">
-		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 container_foto ">
+<div class="container p-1">
+	<div class="row">
+		<div class="col-md-4 container_foto ">
 			<div class="ver_mas text-center">
 				<span class="lnr lnr-eye"></span>
 			</div>
@@ -10,8 +9,9 @@
 				<h4>Descripción corta de la imagen en cuestión</h4>
 			</article>
 			<img src="https://img-aws.ehowcdn.com/400x400/ds-img.studiod.com/Half_Dome_from_Glacier_Point0_1.jpg" alt="">
-		</div>
-		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 container_foto">
+        </div>
+    
+	    <div class="col-md-4 container_foto">
 			<div class="ver_mas text-center">
 				<span id="click" class="comprar">Comprar</span>
 				<span id="click" class="alquilar">Alquilar</span>
@@ -21,8 +21,9 @@
 				<h4>Osa osa osa osa osa osa osa osa osa   osa osa osa osa osa osa osa osa</h4>
 			</article>
 			<img src="https://pbs.twimg.com/profile_images/781518570018648065/HcvZhTVn_400x400.jpg" alt="">
-		</div>
-		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 container_foto">
+        </div>
+        
+		<div class="col-md-4 container_foto">
 			<div class="ver_mas text-center">
 				<span class="lnr lnr-eye"></span>
 			</div>
@@ -31,9 +32,41 @@
 				<h4>Descripción corta de la imagen en cuestión</h4>
 			</article>
 			<img src="https://ericrenardphotography.com/wp-content/uploads/Yosemite_Valley_2861-1-400x400.jpg" alt="">
-		</div>
-	</div>
+        </div>
+        
+        <div class="col-md-4 container_foto">
+			<div class="ver_mas text-center">
+				<span class="lnr lnr-eye"></span>
+			</div>
+			<article class="text-left">
+				<h2>TÍTULO DE <br>LA IMAGEN</h2>
+				<h4>Descripción corta de la imagen en cuestión</h4>
+			</article>
+			<img src="https://ericrenardphotography.com/wp-content/uploads/Yosemite_Valley_2861-1-400x400.jpg" alt="">
+        </div>
+        <div class="col-md-4 container_foto">
+			<div class="ver_mas text-center">
+				<span class="lnr lnr-eye"></span>
+			</div>
+			<article class="text-left">
+				<h2>TÍTULO DE <br>LA IMAGEN</h2>
+				<h4>Descripción corta de la imagen en cuestión</h4>
+			</article>
+			<img src="https://ericrenardphotography.com/wp-content/uploads/Yosemite_Valley_2861-1-400x400.jpg" alt="">
+        </div>
+        <div class="col-md-4 container_foto">
+			<div class="ver_mas text-center">
+				<span class="lnr lnr-eye"></span>
+			</div>
+			<article class="text-left">
+				<h2>TÍTULO DE <br>LA IMAGEN</h2>
+				<h4>Descripción corta de la imagen en cuestión</h4>
+			</article>
+			<img src="https://ericrenardphotography.com/wp-content/uploads/Yosemite_Valley_2861-1-400x400.jpg" alt="">
+        </div>
+    </div>
 </div>
+
 <main class="container h-100 d-flex justify-content-center align-items-center">
     <div>
     <?php

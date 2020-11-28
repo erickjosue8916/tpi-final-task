d319edb860355447874686046d7c045fc4c86eed
EReyes <erk5530@gmail.com>
2020-11-27 21:04:52 -0600

    Modificando estilos

--- views/transacciones/transaccionesList.php
+++ views/transacciones/transaccionesList.php
@@ -1,51 +1,52 @@
 <div class="fondo">
-<main class="container h-100 d-flex justify-content-center align-items-center">
-<div class="container table-responsive py-5 ">
-	<?php
-        $headers = ['#', 'fecha', 'usuario', 'correo', 'total', 'estado', 'tipo', 'Detalles'];
+<main class="container justify-content-center align-items-center">
+		<?php
+			$headers = ['#', 'Fecha', 'Usuario', 'Correo', 'Total', 'Estado', 'Tipo', 'Detalles'];
 
-        $rows = $result['transacciones'];
-        $config = [
-            'headers' => $headers,
-            'rows' => $rows
-		];
-    ?>
-
-    <section class="">
-        <h2>transacciones realizadas</h2>
-	<table class="table table-bordered table-hover table-border">
-	<thead class="thead-dark">
-		<tr>
-		<?php
-		foreach ($headers as $key => $value) {//Por cada título en el arreglo creamos una columna
-			?>
-			<th scope="col"><?=$value?></th>
-			<?php
-		}
+			$rows = $result['transacciones'];
+			$config = [
+				'headers' => $headers,
+				'rows' => $rows
+			];
 		?>
-		</tr>
-	</thead>
-	<tbody>
-	<?php
-		foreach ($rows as $key => $cols) { //Por cada elemento en $rows creamos una fila
-		?>
-		<tr>
-		<?php
-			foreach ($cols as $key => $col) {//Accedemos a los valores que tiene almacenado cada fila uno a uno
-			?>
-				<td scope="col"><?=$col?></td><!--Agregamos ese valor a la fila correspondiente-->
-			<?php
-			}
-			$string = $cols["tipo_transaccion"] == "Compra" ? "detalleCompra":"detalleAlquiler";
-			$string = BASE_DIR."Transacciones/" . $string . "&id=".$cols["id_transaccion"];
-		?>
-        <td scope="col"><a href="<?=$string?>">Detalle</a></td>
-		</tr>
-		<?php
-        }
-	?>
-	</tbody>
-	</table>
-    </section>
-    </main>
+		<section class="">
+			<h2>transacciones realizadas</h2>
+			<table class="table table-dark table-responsive table-striped">
+				<thead>
+					<tr>
+					<?php
+						//Por cada título en el arreglo creamos una columna
+						foreach ($headers as $key => $value) {
+							?>
+							<th scope="col"><?=$value?></th>
+							<?php
+						}
+						?>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					//Por cada elemento en $rows creamos una fila
+					foreach ($rows as $key => $cols) { 
+					?>
+					<tr>
+						<?php
+						//Accedemos a los valores que tiene almacenado cada fila uno a uno
+						foreach ($cols as $key => $col) {
+						?>
+							<td><?=$col?></td><!--Agregamos ese valor a la fila correspondiente-->
+						<?php
+						}
+						$string = $cols["tipo_transaccion"] == "Compra" ? "detalleCompra":"detalleAlquiler";
+						$string = BASE_DIR."Transacciones/" . $string . "&id=".$cols["id_transaccion"];
+						?>
+						<td><a class="btn btn-outline-info text-capitalize" href="<?=$string?>">Detalle</a></td>
+					</tr>
+					<?php
+					}
+					?>
+				</tbody>
+			</table>
+		</section>
+</main>
 </div>

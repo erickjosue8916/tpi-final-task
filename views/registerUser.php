f3a625215291cd18647b60857b864f7c2e9edd71
Nrycord <carlencordero@gmail.com>
2020-11-27 23:11:54 -0600

    Permite agregar usuarios cliente nuevos

--- views/registerUser.php
+++ views/registerUser.php
@@ -6,38 +6,49 @@
         <?=$error;?>
       </div>
     <?php } ?>
-    <form class="login" action="" method="POST">
+    <form class="login" action="<?=BASE_DIR?>Users/register" method="POST">
       <!-- 2 column grid layout with text inputs for the first and last names -->
       <div class="row mb-4">
         <div class="col">
           <div class="form-outline">
-            <input type="text" id="form3Example1" name="name" class="form-control" />
-            <label class="form-label" for="form3Example1">First name</label>
+            <input type="text" id="form3Example1" name="nombre" class="form-control" required="required"/>
+            <label class="form-label" for="form3Example1">Nombre</label>
           </div>
         </div>
         <div class="col">
           <div class="form-outline">
-            <input type="text" id="form3Example2" name="lastname" class="form-control" />
-            <label class="form-label" for="form3Example2">Last name</label>
+            <input type="text" id="form3Example2" name="apellido" class="form-control" required="required"/>
+            <label class="form-label" for="form3Example2">Apellido</label>
           </div>
         </div>
+      </div>
+      
+      <div class="form-outline mb-4">
+        <input type="email" id="form3Example1" name="email" class="form-control" required="required"/>
+        <label class="form-label" for="form3Example1">Correo</label>
+      </div>
+
+      <div class="form-outline mb-4">
+        <input type="text" id="form3Example2" name="telefono" class="form-control" required="required"/>
+        <label class="form-label" for="form3Example2">Telefono</label>
+      </div>
+
+      <div class="form-outline mb-4">
+        <input type="text" id="form3Example3" name="direccion" class="form-control" required="required"/>
+        <label class="form-label" for="form3Example3">Direccion</label>
       </div>
 
       <!-- Email input -->
       <div class="form-outline mb-4">
-        <input type="text" id="form3Example3" name="username" class="form-control" />
-        <label class="form-label" for="form3Example3">Username</label>
+        <input type="text" id="form3Example4" name="username" class="form-control" required="required"/>
+        <label class="form-label" for="form3Example4">Usuario</label>
       </div>
 
-      <div class="form-outline mb-4">
-        <input type="email" id="form3Example3" name="email" class="form-control" />
-        <label class="form-label" for="form3Example3">Email address</label>
-      </div>
 
       <!-- Password input -->
       <div class="form-outline mb-4">
-        <input type="password" id="form3Example4" name="password" class="form-control" />
-        <label class="form-label" for="form3Example4">Password</label>
+        <input type="password" id="form3Example5" name="password" class="form-control" required="required"/>
+        <label class="form-label" for="form3Example5">Password</label>
       </div>
 
       <!-- Submit button -->

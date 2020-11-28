267e282983fc4c5ec95b08b02a47d1be72860d75
EReyes <erk5530@gmail.com>
2020-11-27 23:17:05 -0600

    Modificando estilos

--- views/transacciones/transaccionesList.php
+++ views/transacciones/transaccionesList.php
@@ -1,52 +1,56 @@
 <div class="fondo">
-<main class="container justify-content-center align-items-center">
-		<?php
-			$headers = ['#', 'Fecha', 'Usuario', 'Correo', 'Total', 'Estado', 'Tipo', 'Detalles'];
+	<main class="container">
+		<div class="row h-100 justify-content-center align-items-center">
+			<div class="col">
+			<?php
+				$headers = ['#', 'Fecha', 'Usuario', 'Correo', 'Total', 'Estado', 'Tipo', 'Detalles'];
 
-			$rows = $result['transacciones'];
-			$config = [
-				'headers' => $headers,
-				'rows' => $rows
-			];
-		?>
-		<section class="">
-			<h2>transacciones realizadas</h2>
-			<table class="table table-dark table-responsive table-striped">
-				<thead>
-					<tr>
-					<?php
-						//Por cada título en el arreglo creamos una columna
-						foreach ($headers as $key => $value) {
+				$rows = $result['transacciones'];
+				$config = [
+					'headers' => $headers,
+					'rows' => $rows
+				];
+			?>
+			<h1 class="text-center mb-5 font-weight-light text-white">Transacciones realizadas</h1>
+			<section class="table-responsive">
+				<table class="table-styles">
+					<thead>
+						<tr>
+						<?php
+							//Por cada título en el arreglo creamos una columna
+							foreach ($headers as $key => $value) {
+								?>
+								<th scope="col"><h1><?=$value?></h1></th>
+								<?php
+							}
 							?>
-							<th scope="col"><?=$value?></th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php
+						//Por cada elemento en $rows creamos una fila
+						foreach ($rows as $key => $cols) { 
+						?>
+						<tr>
 							<?php
-						}
-						?>
-					</tr>
-				</thead>
-				<tbody>
-					<?php
-					//Por cada elemento en $rows creamos una fila
-					foreach ($rows as $key => $cols) { 
-					?>
-					<tr>
-						<?php
-						//Accedemos a los valores que tiene almacenado cada fila uno a uno
-						foreach ($cols as $key => $col) {
-						?>
-							<td><?=$col?></td><!--Agregamos ese valor a la fila correspondiente-->
+							//Accedemos a los valores que tiene almacenado cada fila uno a uno
+							foreach ($cols as $key => $col) {
+							?>
+								<td><?=$col?></td><!--Agregamos ese valor a la fila correspondiente-->
+							<?php
+							}
+							$string = $cols["tipo_transaccion"] == "Compra" ? "detalleCompra":"detalleAlquiler";
+							$string = BASE_DIR."Transacciones/" . $string . "&id=".$cols["id_transaccion"];
+							?>
+							<td><a class="btn btn-outline-info text-capitalize" href="<?=$string?>">Detalle</a></td>
+						</tr>
 						<?php
 						}
-						$string = $cols["tipo_transaccion"] == "Compra" ? "detalleCompra":"detalleAlquiler";
-						$string = BASE_DIR."Transacciones/" . $string . "&id=".$cols["id_transaccion"];
 						?>
-						<td><a class="btn btn-outline-info text-capitalize" href="<?=$string?>">Detalle</a></td>
-					</tr>
-					<?php
-					}
-					?>
-				</tbody>
-			</table>
-		</section>
-</main>
+					</tbody>
+				</table>
+			</section>
+			</div>
+		</div>	
+	</main>
 </div>
